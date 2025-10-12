5ec0d0022bd6cb2e8d998cfdc41a2387c4d5b00a
Sai Kiran Kondapalli <saikirankondapalli03@gmail.com>
2025-10-12 09:50:00 -0400

    make it readable

---
+++ educativepatterns/src/patterns/dfs/TreeNode.java
@@ -0,0 +1,13 @@
+package patterns.dfs;
+
+class TreeNode {
+	  int val;
+	  TreeNode left;
+	  TreeNode right;
+
+	  TreeNode(int x) {
+	    val = x;
+	  }
+	};
+
+	
---
+++ educativepatterns/src/patterns/slidingwindow/pattern3/stringwindow/Print.java
@@ -0,0 +1,23 @@
+package patterns.slidingwindow.pattern3.stringwindow;
+
+import java.util.Set;
+
+public class Print {
+    public static String printSetString(Set<String> set) {
+        if (set == null || set.isEmpty()) {
+            return "[]";
+        }
+        StringBuilder sb = new StringBuilder("[");
+        for (String s : set) {
+            sb.append(s).append(", ");
+        }
+        sb.setLength(sb.length() - 2); // Remove last ", "
+        sb.append("]");
+        return sb.toString();
+    }
+    
+    public static String repeat(String str, int count) {
+        return new String(new char[count]).replace("\0", str);
+    }
+
+}
