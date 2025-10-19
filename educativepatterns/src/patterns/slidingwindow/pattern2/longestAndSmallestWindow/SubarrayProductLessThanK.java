b8b3fcc59c7db93eca98e63449147e9a39fbb916
Sai Kiran Kondapalli <saikirankondapalli03@gmail.com>
2025-10-19 02:44:49 -0400

    moved the code to right pattern

--- educativepatterns/src/patterns/slidingwindow/pattern2/longestAndSmallestWindow/SubarrayProductLessThanK.java
+++ educativepatterns/src/patterns/slidingwindow/pattern2/longestAndSmallestWindow/SubarrayProductLessThanK.java
@@ -1,4 +1,4 @@
-package patterns.twopointers;
+package patterns.slidingwindow.pattern2.longestAndSmallestWindow;
 
 import java.util.*;
 
@@ -29,7 +29,36 @@ class SubarrayProductLessThanK {
 	}
 
 	public static void main(String[] args) {
+		SubarrayProductLessThanK.printSubarrays(new int[] { 8, 2, 6, 5 }, 50);
+
+		
 		System.out.println(SubarrayProductLessThanK.findSubarrays(new int[] { 2, 5, 3, 10 }, 30));
+	
 		System.out.println(SubarrayProductLessThanK.findSubarrays(new int[] { 8, 2, 6, 5 }, 50));
 	}
+	
+	
+    public static void printSubarrays(int[] nums, int k) {
+        if (k <= 1) return;
+
+        int left = 0;
+        long product = 1;
+
+        for (int right = 0; right < nums.length; right++) {
+            product *= nums[right];
+
+            // shrink window if invalid
+            while (product >= k && left <= right) {
+                product /= nums[left++];
+            }
+
+            // collect subarrays ending at right
+            List<Integer> temp = new ArrayList<>();
+            for (int i = right; i >= left; i--) {
+                temp.add(0, nums[i]); // prepend to maintain order
+            }
+            System.out.println(temp);
+
+        }
+    }
 }
